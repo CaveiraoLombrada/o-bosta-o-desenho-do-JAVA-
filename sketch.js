de1de9051fad4a59379be1ea0a86a5dbcc2da882
CaveiraoLombrada <ferreiradesouzaotavio899@gmail.com>
2024-05-23 16:22:41 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+functionunction setup() {
+  createCanvas(800, 500);
+  background("black");
+}
+
+function draw() {
+  fill("pink");
+  stroke("rgb(255,255,255) ");
+  if(mouseIsPressed);
+  circle(mouseX, mouseY, 30, 50 );
+}
